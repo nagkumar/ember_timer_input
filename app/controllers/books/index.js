9f6eec6f89b3d157d31907141029f8e3314df063
rnkumar <raja.kumar@certesnetworks.com>
2016-03-18 08:08:09 +0530

    timer example..

--- app/controllers/books/index.js
+++ app/controllers/books/index.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 import groupBy from 'ember-group-by';
 
-export default Ember.Controller.extend({
-                                         booksByType: groupBy('model', 'type')
-                                       });
+export default Ember.Controller.extend(
+  {
+    booksByType: groupBy('model', 'type'),
+    time4: moment("11:21 p.m", 'hh:mm a')
+});
