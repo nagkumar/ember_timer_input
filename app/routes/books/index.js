fa79c819ef80a83ee945676909da27b367938e1c
rnkumar <raja.kumar@certesnetworks.com>
2016-03-18 08:08:09 +0530

    timer example..

--- app/routes/books/index.js
+++ app/routes/books/index.js
@@ -1,8 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-                                    model: function ()
-                                    {
-                                      return this.store.findAll('book');
-                                    }
-                                  });
+  model: function ()
+  {
+    var m = this.store.findAll('book');
+    return m;
+  }
+});
